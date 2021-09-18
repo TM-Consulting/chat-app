c63bfc328a368c3a17cade2833c561ba0fe68a21
TM-Consulting <mounaim.touinssi@gmail.com>
2021-09-18 14:43:05 +0100

    rename container

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import SignIn from "./pages/signIn";
-import SignUp from "./pages/signUp";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
