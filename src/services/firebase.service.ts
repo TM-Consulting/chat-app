41dee82fdd1889e757fa4dafc979e70dd3888a2b
karima-dev <karimakhadir1@gmail.com>
2021-09-20 18:29:17 +0100

    commit firebase

--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -1,22 +1,22 @@
 import firebase from "../firebase";
 
-const db = firebase.ref("/profile");
-
-export const FirebaseProfile=()=> {
-    const getAll = () => {
+ 
+export const FirebaseProfile=(provider:any)=> {
+  const db = firebase.ref(provider);
+    return {
+       getAll : () => {
         return db;
-      };
-      const create = (data:any) => {
+      },
+       create : (data:any) => {
         return db.add(data);
-      };
-      const update = (id:any, value:any) => {
+      },
+       update : (id:any, value:any) => {
         return db.doc(id).update(value);
-      };
-      const remove = (id:any) => {
+      },
+       remove : (id:any) => {
         return db.doc(id).delete();
-      };
-    
-      return { getAll, create, update, remove };
+      },
+    }    
 };
 
  
