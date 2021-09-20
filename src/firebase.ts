cc1a81136f2105068976fd6fbe223a525d8c0e8f
TM-Consulting <mounaim.touinssi@gmail.com>
2021-09-20 19:27:39 +0100

    add env file

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,13 +1,8 @@
 import firebase from "firebase";
 import "firebase/database";
- let config = {
-    apiKey: "AIzaSyAV9hGXqBuAVaUj9saG30r-qy-2uXNXtXI",
-    authDomain: "chat-app-c5f12.firebaseapp.com",
-    databaseURL: "https://chat-app-c5f12-default-rtdb.firebaseio.com",
-    projectId: "chat-app-c5f12",
-    storageBucket: "chat-app-c5f12.appspot.com",
-    messagingSenderId: "784028167085",
-    appId: "1:784028167085:web:ccc209dfeb3c71e2008f24"
+import { firebaseConfig } from "./variables";
+let config = {
+  ...firebaseConfig,
 };
 
 firebase.initializeApp(config);
