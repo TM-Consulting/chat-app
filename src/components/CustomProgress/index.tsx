01809ba881e217469f1927b4c88a4fc0dbf16313
rush2di <roch2di@gmail.com>
2021-09-25 13:32:37 +0100

    add conditional progress render

--- src/components/CustomProgress/index.tsx
+++ src/components/CustomProgress/index.tsx
@@ -2,25 +2,38 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { CircularProgressProps } from "@material-ui/core/CircularProgress";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    width: "100%",
+  },
+  elem: {
+    display: "flex",
     flexDirection: "column",
-    "& > * + *": {
-      marginLeft: theme.spacing(2),
-    },
+    alignItems: "center",
+  },
+  text: {
+    marginTop: theme.spacing(2),
+    display: "inline-block",
   },
 }));
+
 const CustomProgress = ({
   text,
   ...circularProps
 }: CustomProgressProps & CircularProgressProps) => {
-  const classes = useStyles();
+  const { root, elem, text: textClass } = useStyles();
 
   return (
-    <div className={classes.root}>
-      <CircularProgress {...circularProps} />
-      <span>{text}</span>
+    <div className={root}>
+      <div className={elem}>
+        <CircularProgress {...circularProps} />
+        <span className={textClass}>{text}</span>
+      </div>
     </div>
   );
 };
--- src/containers/ChatBody/index.tsx
+++ src/containers/ChatBody/index.tsx
@@ -1,21 +1,32 @@
-import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import Divider from "@material-ui/core/Divider";
+import { useSelector } from "react-redux";
+import { createStructuredSelector } from "reselect";
 import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
+
+import CustomProgress from "../../components/CustomProgress";
+import CustomGrid from "../../components/CustomGrid";
 import { customGridProps } from "../../constants";
 import { styles } from "../../constants";
-import CustomGrid from "../../components/CustomGrid";
+import { makeSelectLoading } from "./selectors";
+
 const useStyles = makeStyles({ ...styles });
 
+const chatState = createStructuredSelector({
+  loading: makeSelectLoading(),
+});
+
 const ChatBody = () => {
+  const { loading } = useSelector(chatState);
+
   const classes = useStyles();
+
   return (
     <List className={classes.messageArea}>
-      {customGridProps.map((item) => (
-        <CustomGrid {...item} />
-      ))}
+      {loading === true ? (
+        <CustomProgress />
+      ) : (
+        customGridProps.map((item) => <CustomGrid {...item} />)
+      )}
     </List>
   );
 };
